aa75fa8ca3950e8df6458fcecdbdafbf00d7a806
brittnym <brittnymartin@gmail.com>
2022-03-10 11:30:21 -0500

    Output a string showing results of the 'in range' method.

--- MethodsExamples/Program.cs
+++ MethodsExamples/Program.cs
@@ -4,16 +4,37 @@ namespace MethodsExamples
 {
     public class Program
     {
-        //main method. This is where you put only the essential stuff. Like, //
+        //main method. This is where you put only the essential stuff. //
         static void Main(string[] args)
         {
-           //TODO: implement method to check if a number is in range of another int. That's what the first method below is
-          
+            //TODO: implement method to check if a number is in range of another int. That's what the first method below is
+
+            //TODO: ask the user for the initial number
+            //TODO: ask the user for the target 
+            //TODO: ask the user for the range
+            //None of the infor in this method needs to be another method because you're not doing anything but asking questions and printing output.
+
+            Console.WriteLine("Enter an initial number: ");
+            int StartingNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an taregt number: ");
+            int TargetNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an range: ");
+            int range = int.Parse(Console.ReadLine());
+            
+            //TODO: write the result by calling IsInRange
+            Console.WriteLine($"Given a starting number of {StartingNum}, and a target number of {TargetNum},it's {IsWithinRange(StartingNum, TargetNum, range)} ");
+
+
+
+
         }
         //access modifier followed by static or not followed by return type, then name, then (), then parameters
-        private static bool IsWithinRange(int num1, int num2, int range)
+        private static bool IsWithinRange(int num, int target, int range)
         {
-            return (num1 - num2) <= range;
+            return (num - target) <= range;
         }
+
     }
 }
