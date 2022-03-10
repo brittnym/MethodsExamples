bab69f24fab0dbd7d3afad829463bd4de8a13fae
brittnym <brittnymartin@gmail.com>
2022-03-10 12:05:51 -0500

    Update formula of IsWithinRange()

--- MethodsExamples/Program.cs
+++ MethodsExamples/Program.cs
@@ -9,31 +9,33 @@ namespace MethodsExamples
         {
             //TODO: implement method to check if a number is in range of another int. That's what the first method below is
 
-            //TODO: ask the user for the initial number
-            //TODO: ask the user for the target 
-            //TODO: ask the user for the range
+
             //None of the infor in this method needs to be another method because you're not doing anything but asking questions and printing output.
 
+            //TODO: ask the user for the initial number
             Console.WriteLine("Enter an initial number: ");
             int StartingNum = int.Parse(Console.ReadLine());
 
+            //TODO: ask the user for the target 
             Console.WriteLine("Enter an taregt number: ");
             int TargetNum = int.Parse(Console.ReadLine());
 
+            //TODO: ask the user for the range
             Console.WriteLine("Enter an range: ");
             int range = int.Parse(Console.ReadLine());
             
             //TODO: write the result by calling IsInRange
             Console.WriteLine($"Given a starting number of {StartingNum}, and a target number of {TargetNum},it's {IsWithinRange(StartingNum, TargetNum, range)} ");
 
-
-
-
         }
         //access modifier followed by static or not followed by return type, then name, then (), then parameters
         private static bool IsWithinRange(int num, int target, int range)
         {
-            return (num - target) <= range;
+            //This particular math doesn't work properly, but for the purposes of this exercise, it doesn't really matter
+           // return (num - target) <= range;//
+
+            //this is actually the correct formula
+            return (num - target) <= range && (num - target) >= -range;
         }
 
     }
