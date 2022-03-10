4259b2801c323bf5afca170758933ed57a309315
brittnym <brittnymartin@gmail.com>
2022-03-10 10:51:51 -0500

    implement method to check if a number is in range of another int

---
+++ MethodsExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodsExamples
+{
+    public class Program
+    {
+        //main method. This is where you put only the essential stuff. Like, //
+        static void Main(string[] args)
+        {
+           //TODO: implement method to check if a number is in range of another int. That's what the first method below is
+          
+        }
+        //access modifier followed by static or not followed by return type, then name, then (), then parameters
+        private static bool IsWithinRange(int num1, int num2, int range)
+        {
+            return (num1 - num2) <= range;
+        }
+    }
+}
